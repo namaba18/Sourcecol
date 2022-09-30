f4e8197623667314750da093230201854ef89b10
NATALIA MANTILLA BARRIENTOS <nataliamantilla307633@correo.itm.edu.co>
2022-09-30 15:58:09 -0500

    Metodo Cadena

--- PruebaTecnica/PruebaTecnica/Program.cs
+++ PruebaTecnica/PruebaTecnica/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(Cadena("asdfjj"));
+
+string Cadena(string cad)
+{
+    string cad2 = "";
+    if (cad.Length <= 20)
+    {
+        for (int i = 0; i < cad.Length; i++)
+        {
+            cad2 += cad[i];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            cad2 += cad[i];
+        }
+        cad2 += "...";
+    }
+    return cad2;
+}
