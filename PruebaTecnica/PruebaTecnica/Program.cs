6d0f653eb106d4751d2740f91baf86a25cb35374
NATALIA MANTILLA BARRIENTOS <nataliamantilla307633@correo.itm.edu.co>
2022-09-30 16:26:47 -0500

    Merge

--- PruebaTecnica/PruebaTecnica/Program.cs
+++ PruebaTecnica/PruebaTecnica/Program.cs
@@ -51,6 +51,43 @@ namespace PruebaTecnica
 
             return temp;
         }
+        static string Pares(int[] serie)
+        {
+            List<int> pares = new List<int>();
+            int[] arreglo;
+            string text = "El arreglo de números pares ordenados de mayor a menor es: [";
+            foreach (int i in serie)
+            {
+                if (i % 2 == 0)
+                {
+                    pares.Add(i);
+                }
+            }
+            arreglo = pares.ToArray();
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                for (int j = i + 1; j < (arreglo.Length); j++)
+                {
+                    if (arreglo[i] < arreglo[j])
+                    {
+                        int temp = arreglo[i];
+                        arreglo[i] = arreglo[j];
+                        arreglo[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                text += arreglo[i];
+                if (i < arreglo.Length - 1)
+                {
+                    text += ", ";
+                }
+            }
+            text += "] y tiene " + arreglo.Length + " elementos";
+
+            return text;
+        }
 
         static void Main(string[] args)
         {
@@ -70,7 +107,7 @@ namespace PruebaTecnica
 
             Console.WriteLine(Fibonacci(vect));
 
-
+            Console.WriteLine(Pares(vect));
         }
     }
 
@@ -88,44 +125,4 @@ namespace PruebaTecnica
 
 
 
-int[] vect = { 1, 1, 2, 3, 6, 8, 10 };
 
-Console.WriteLine(Pares(vect));
-
-string Pares(int[] serie)
-{
-    List<int> pares = new List<int>();
-    int[] arreglo;
-    string text = "El arreglo de números pares ordenados de mayor a menor es: [";
-    foreach (int i in serie)
-    {
-        if (i % 2 == 0)
-        {
-            pares.Add(i);
-        }
-    }
-    arreglo = pares.ToArray();
-    for (int i = 0; i < arreglo.Length; i++)
-    {
-        for (int j = i + 1; j < (arreglo.Length); j++)
-        {
-            if (arreglo[i] < arreglo[j])
-            {
-                int temp = arreglo[i];
-                arreglo[i] = arreglo[j];
-                arreglo[j] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < arreglo.Length; i++)
-    {
-        text += arreglo[i];
-        if (i < arreglo.Length - 1)
-        {
-            text += ", ";
-        }
-    }
-    text += "] y tiene " + arreglo.Length + " elementos";
-
-    return text;
-}
