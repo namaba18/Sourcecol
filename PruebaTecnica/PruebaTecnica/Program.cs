b5f77162d1fb96175f485a6aec20ee6b5c3284c4
NATALIA MANTILLA BARRIENTOS <nataliamantilla307633@correo.itm.edu.co>
2022-09-30 15:11:51 -0500

    Metodo de Fibonacci

--- PruebaTecnica/PruebaTecnica/Program.cs
+++ PruebaTecnica/PruebaTecnica/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] vect = { 1, 1, 2, 3, 5, 8, 13 };
+
+Console.WriteLine(Fibonacci(vect));
+
+
+string Fibonacci(int[] serie)
+{
+    string temp = "Cumple con la sucesión de Fibonacci";
+    for (int i = 0; i < (serie.Length - 2); i++)
+    {
+        if (serie[i] != serie[i + 2] - serie[i + 1])
+        {
+            temp = "No cumple con la suceción de Fibonacci";
+        }
+    }
+
+    return temp;
+}
